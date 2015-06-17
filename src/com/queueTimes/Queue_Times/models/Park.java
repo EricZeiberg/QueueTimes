5f6b3890791800eda04a246e81f74f419c38809b
Eric Zeiberg <ejzeiberg@gmail.com>
2015-06-17 13:21:55 -0400

    Add constructer and getters to Park model

--- src/com/queueTimes/Queue_Times/models/Park.java
+++ src/com/queueTimes/Queue_Times/models/Park.java
@@ -12,4 +12,37 @@ public class Park {
     int ID;
 
     List<Ride> rides = new ArrayList<Ride>();
+
+    public Park(String name, String description, String uri, String country, int ID, List<Ride> rides) {
+        this.name = name;
+        this.description = description;
+        this.uri = uri;
+        this.country = country;
+        this.ID = ID;
+        this.rides = rides;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public List<Ride> getRides() {
+        return rides;
+    }
 }
